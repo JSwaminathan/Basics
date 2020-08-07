46c1df4c00fc3bb4b21dcfe03e547631d07f3010
AshwinJayaram <66546960+AshwinJayaram@users.noreply.github.com>
2020-08-07 16:37:09 +0530

    Revert "Delete MybatisIntro.java"
    This reverts commit e11036a2ebc1571e70549ca52c021012c023fc10.

---
+++ programming/MybatisIntro.java
@@ -0,0 +1,36 @@
+package programming;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ *  Mybatis basics - SELECT statement
+ */
+public class MybatisIntro {
+
+	public static void main(String args[]) throws IOException {
+
+		Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		SqlSession session = sqlSessionFactory.openSession();
+
+		// Retrieve all the students.
+		// This uses a pooled connection for the mysql database.
+		List<Student> student = session.selectList("Student.getAll");
+
+		for (Student st : student) {
+			System.out.println(st.getId() + " " + st.getName() + " " + st.getEmail());
+	}
+
+		System.out.println("Records Read Successfully ");
+		session.commit();
+		session.close();
+	}
+
+}

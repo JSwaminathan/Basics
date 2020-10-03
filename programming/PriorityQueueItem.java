ec0739dcb957975d44759e9c8abb087c613e36cd
AshwinJayaram <66546960+AshwinJayaram@users.noreply.github.com>
2020-10-03 14:56:30 +0530

    fexible priorirty queue

--- programming/PriorityQueueItem.java
+++ programming/PriorityQueueItem.java
@@ -5,7 +5,7 @@ import programming.PriorityQueue.Priority;
 public class PriorityQueueItem extends QueueItem {
 
 	protected Priority priority;
-	
+
 	public PriorityQueueItem(Object value, Priority priority) {
 		super(value);
 		this.priority = priority;

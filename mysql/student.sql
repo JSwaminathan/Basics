9a546b16704ee610d175869154a516c43ce49569
Arjun j <66547015+arjun-jayaram@users.noreply.github.com>
2020-10-04 14:26:55 +0530

    changed student.xml

--- mysql/student.sql
+++ mysql/student.sql
@@ -7,4 +7,4 @@ CREATE TABLE `details`.`student` (
 INSERT INTO `details`.`student` (`ID`, `name`, `email`, `phone`) VALUES ('1', 'ashwin', 'ashwin@gmail.com', '8939386198');
 INSERT INTO `details`.`student` (`ID`, `name`, `email`, `phone`) VALUES ('2', 'arjun', 'arjun@gmail.com', '9884991987');
 INSERT INTO `details`.`student` (`ID`, `name`, `email`, `phone`) VALUES ('3', 'meenakshi', 'm@gmail.com', '9567101987');
-INSERT INTO `details`.`student` (`ID`, `name`, `email`, `phone`) VALUES ('4', 'j', 'j@gmail.com', '9789833077');
+INSERT INTO `details`.`student` (`ID`, `name`, `email`, `phone`) VALUES ('4', 'jayaram', 'j@gmail.com', '9789833077');

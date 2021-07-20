8b5585ff272b44a2ab9c979a4d1f424200144258
AshwinJayaram <66546960+AshwinJayaram@users.noreply.github.com>
2021-07-20 13:05:32 +0530

    LOGGING

--- com/jaa/billing/BillingSolution.java
+++ com/jaa/billing/BillingSolution.java
@@ -1,5 +1,18 @@
 package com.jaa.billing;
 
-public class BillingSolution {
+import org.apache.log4j.Logger;
 
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+
+public class BillingSolution {
+	/* Get the class name to be printed on */
+	static Logger log = Logger.getLogger(BillingSolution.class.getName());
+
+	public static void main(String[] args) throws IOException, SQLException {
+		log.debug("Initializing gst calculator");
+		log.info("Hello this is an info message");
+		log.warn("gst not wrriten");
+	}
 }

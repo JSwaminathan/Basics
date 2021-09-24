bb09af059cd9650ab1417d3ec1054864411e53b2
AshwinJayaram <66546960+AshwinJayaram@users.noreply.github.com>
2021-09-24 13:11:32 +0530

    threading

--- programming/Threading.java
+++ programming/Threading.java
@@ -1,7 +1,7 @@
 package programming;
 
 public class Threading {
-	public static final int MAX = 20;
+	public static final int MAX = 5000;
 
 	public static void main(String[] args) {
 		MyRunner run1 = new MyRunner(1);
@@ -9,19 +9,25 @@ public class Threading {
 		MyRunner run3 = new MyRunner(3);
 		MyRunner run4 = new MyRunner(4);
 		MyRunner run5 = new MyRunner(5);
-		
+
 		System.out.println("Start Time: " + (new java.util.Date()).toString());
-		run1.start();
-		run2.start();
-		run3.start();
-		run4.start();
-		run5.start();
-		
+		Thread t1 = new Thread(run1);
+		Thread t2 = new Thread(run2);
+		Thread t3 = new Thread(run3);
+		Thread t4 = new Thread(run4);
+		Thread t5 = new Thread(run5);
+
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+
 		System.out.println("End Time: " + (new java.util.Date()).toString());
 	}
 }
 
-class MyRunner extends Thread {
+class MyRunner implements Runnable {
 	int myNumber;
 
 	MyRunner(int myNumber) {
